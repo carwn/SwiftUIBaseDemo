fac7911ba5927eb978477a4e1e5712e0a2ab7eeb
Alexander Shelikhov <carwn@bk.ru>
2022-11-20 13:02:07 +0300

    Initial Commit

---
+++ SwiftUIBaseDemo/SwiftUIBaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseDemoApp.swift
+//  SwiftUIBaseDemo
+//
+//  Created by Александр Шелихов on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
